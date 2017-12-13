1ae10df06a710c3c0d2f4ad7a19587b9c97c9e67
nre <nre@ableton.com>
2017-12-13 19:46:12 +0100

    Don't used named arguments when calling run
    For some reason, Jenkins refuses to execute `run` when using named
    arguments. There is no stacktrace and my groovy-fu is not good enough
    to understand why it would fail. Regardless, the call also works fine
    without named arguments.

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,7 +1,7 @@
 def create(String python, String path = "venv", boolean installRequirements = true) {
   sh("virtualenv -p ${python} ${path}")
   if (installRequirements && fileExists("requirements.txt")) {
-    run(path:path, command:"pip install -r requirements.txt")
+    run(path, "pip install -r requirements.txt")
   }
 }
 
