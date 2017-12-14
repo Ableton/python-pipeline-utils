5c303b88deb911fa728110fe5d927b4a9ab09719
nre <nre@ableton.com>
2017-12-14 11:34:43 +0100

    Use long form for --python argument

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,5 +1,5 @@
 def create(String python, String path = "venv", boolean installRequirements = true) {
-  sh("virtualenv -p ${python} ${path}")
+  sh("virtualenv --python=${python} ${path}")
   if (installRequirements && fileExists("requirements.txt")) {
     run(path, "pip install -r requirements.txt")
   }
