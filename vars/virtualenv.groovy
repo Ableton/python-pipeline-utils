30c9995b66160cdd347d91047eda290dda28a0a5
nre <nre@ableton.com>
2017-12-14 11:36:01 +0100

    Rename path to destDir, remove default value

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,14 +1,14 @@
-def create(String python, String path = "venv", boolean installRequirements = true) {
-  sh("virtualenv --python=${python} ${path}")
+def create(String python, String destDir, boolean installRequirements = true) {
+  sh("virtualenv --python=${python} ${destDir}")
   if (installRequirements && fileExists("requirements.txt")) {
-    run(path, "pip install -r requirements.txt")
+    run(destDir, "pip install -r requirements.txt")
   }
 }
 
 
-def run(String path = "venv", String command) {
+def run(String destDir, String command) {
   sh("""
-    . ./${path}/bin/activate
+    . ./${destDir}/bin/activate
     ${command}
   """)
 }
