710b3a978b3362b87393d0aadd1570f87109f652
nre <nre@ableton.com>
2017-12-14 16:45:38 +0100

    Introduce VirtualEnv class
    This commit introduces a class for VirtualEnv which keeps an internal
    reference to the virtualenv destination dir. Also note that this
    directory is no longer configurable, but instead is safely tucked away
    in a temporary directory.
    
    The virtualenv singleton has also been changed to remove all functions
    other than create(), which now returns a new instance of the
    VirtualEnv class.

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,16 +1,8 @@
-def create(String python, String destDir) {
-  sh("virtualenv --python=${python} ${destDir}")
-}
+import com.ableton.VirtualEnv
 
 
-def installRequirements(String destDir) {
-  run(destDir, "pip install -r requirements.txt")
-}
-
-
-def run(String destDir, String command) {
-  sh("""
-    . ./${destDir}/bin/activate
-    ${command}
-  """)
+def create(script, String python) {
+  venv = new VirtualEnv(script, python)
+  venv.script.sh("virtualenv --python=${python} ${venv.destDir}")
+  return venv
 }
