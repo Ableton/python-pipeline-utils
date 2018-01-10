d660804f3f7269e359b656c6844009da78445e32
nre <nre@ableton.com>
2018-01-10 13:44:23 +0100

    Mock Jenkins' pwd function

--- test/com/ableton/JenkinsMocks.groovy
+++ test/com/ableton/JenkinsMocks.groovy
@@ -9,4 +9,8 @@ class JenkinsMocks {
   static Closure isUnix = {
     return !System.properties['os.name'].toLowerCase().contains('windows')
   }
+
+  static Closure pwd = { args ->
+    return System.properties[args?.tmp ? 'java.io.tmpdir' : 'user.dir']
+  }
 }
--- test/com/ableton/JenkinsMocksTest.groovy
+++ test/com/ableton/JenkinsMocksTest.groovy
@@ -4,6 +4,9 @@ import com.lesfurets.jenkins.unit.BasePipelineTest
 import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+
 
 /**
  * Tests for the JenkinsMocks class.
@@ -20,5 +23,21 @@ class JenkinsMocksTest extends BasePipelineTest {
     // It would be pretty redundant to basically re-implement this method in its own test
     // case, so instead we just call the function and see that it didn't go haywire.
     JenkinsMocks.isUnix()
+  }
+
+  @Test
+  void pwd() throws Exception {
+    String result = JenkinsMocks.pwd()
+    assertNotNull(result)
+    File f = new File(result)
+    assertTrue(f.exists())
+  }
+
+  @Test
+  void pwdTmp() throws Exception {
+    String result = JenkinsMocks.pwd(tmp: true)
+    assertNotNull(result)
+    File f = new File(result)
+    assertTrue(f.exists())
   }
 }
