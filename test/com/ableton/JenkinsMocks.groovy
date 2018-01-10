55d77816c43748f6a93536dc49232dfd889f7315
nre <nre@ableton.com>
2018-01-10 13:44:23 +0100

    Add stubs for JenkinsMocks class, test class

---
+++ test/com/ableton/JenkinsMocks.groovy
@@ -0,0 +1,9 @@
+package com.ableton
+
+
+/**
+ * Provides functional mocks for some Jenkins functions, which is useful in combination
+ * with the JenkinsPipelineUnit library.
+ */
+class JenkinsMocks {
+}
---
+++ test/com/ableton/JenkinsMocksTest.groovy
@@ -0,0 +1,16 @@
+package com.ableton
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+
+
+/**
+ * Tests for the JenkinsMocks class.
+ */
+class JenkinsMocksTest extends BasePipelineTest {
+  @Override
+  @Before
+  void setUp() {
+    super.setUp()
+  }
+}
