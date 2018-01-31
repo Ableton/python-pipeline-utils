ce89250b76d7a897be4f457ea68b0c8a40835b8b
nre <nre@ableton.com>
2018-01-31 16:53:49 +0100

    Introduce new class: PythonBuilder
    The PythonBuilder class will download a Python tarball from the
    internet, extract it to the job's temporary directory, compile it, and
    then return a string to the installation directory (also located in
    the job's temporary directory). This takes about two minutes on a
    modestly-powered Linux node, but can be sped up with the 'makeJobs'
    argument.
    
    A corresponding singleton has also been for easy access from
    Jenkinsfiles.

---
+++ src/com/ableton/PythonBuilder.groovy
@@ -0,0 +1,101 @@
+package com.ableton
+
+
+class PythonBuilder implements Serializable {
+  @SuppressWarnings('FieldTypeRequired')
+  def script = null
+  /**
+   * Python major version to download (required value). Example: '3.7.0'
+   */
+  String version = null
+
+  /**
+   * Mirror to use when downloading Python.
+   */
+  String downloadMirror = 'https://www.python.org/ftp/python'
+  /**
+   * Method to use to download Python. Currently supported methods include:
+   * - 'curl': Requires curl to be installed on the build node.
+   * - 'httpRequest': Requires the Jenkins HTTP Request Plugin to be installed (default).
+   */
+  String downloadWith = 'httpRequest'
+  /**
+   * Number of parallel jobs to use when building Python (ie, `make -jN`). Default: '1'
+   */
+  String makeJobs = '1'
+  /**
+   * Python version revision, optional. This value is appended to the major version, but
+   * because of the way that Python's downloads are organized, this value must be separate
+   * from the major version. So for Python 3.7.0a4, use '3.7.0' for the version and 'a4'
+   * for the revision.
+   */
+  String revision = ''
+
+  String install() {
+    // Sanity check required values
+    assert script
+    assert version
+
+    if (!script.isUnix()) {
+      throw new UnsupportedOperationException('This function is only supported on Unix')
+    }
+
+    String sourcesTarball = download()
+    String sourcesPath = extract(sourcesTarball)
+    return build(sourcesPath)
+  }
+
+  protected String pythonBaseName() {
+    return 'Python-' + version + revision
+  }
+
+  protected String tempDir() {
+    return script.pwd(tmp: true)
+  }
+
+  protected String download() {
+    String downloadUrl = downloadMirror + '/' + version +
+      '/' + pythonBaseName() + '.tgz'
+    String outputFile = script.pwd(tmp: true) + '/' + pythonBaseName() + '.tgz'
+    script.echo 'Downloading python from: ' + downloadUrl
+
+    switch (downloadWith) {
+      case 'curl':
+        script.sh "curl -s -o ${outputFile} ${downloadUrl}"
+        break
+      case 'httpRequest':
+        script.httpRequest(outputFile: outputFile, url: downloadUrl)
+        break
+      default:
+        script.error 'Unknown downloadWith type: ' + downloadWith
+    }
+
+    return outputFile
+  }
+
+  protected String extract(String sourcesTarball) {
+    script.echo 'Extracting python sources'
+    script.dir(tempDir()) {
+      script.sh 'tar xfz ' + sourcesTarball
+    }
+    return tempDir() + '/' + pythonBaseName()
+  }
+
+  protected String build(String sourcesPath) {
+    // Ensure that makeJobs is not an empty string, since make is a bit dumb and running
+    // `make -j` will spawn one job for every file, potentially forkbombing the node.
+    if (!makeJobs || !makeJobs.toInteger()) {
+      throw new IllegalArgumentException('Invalid argument for makeJobs: ' + makeJobs)
+    }
+
+    script.echo 'Building python from: ' + sourcesPath
+    String installPath = script.pwd(tmp: true) + '/' + pythonBaseName()
+    script.dir(sourcesPath) {
+      script.sh './configure --prefix=' + installPath
+      script.sh 'make -j' + makeJobs
+      script.sh 'make install'
+      script.deleteDir()
+    }
+    return installPath
+  }
+}
