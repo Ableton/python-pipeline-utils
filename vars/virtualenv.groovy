c06bfdf625f82a221041cf7aca7cec6ee5735509
rco <rco@ableton.com>
2020-01-30 15:20:15 +0100

    Allow Python versions to be loaded via pyenv
    Overloads the create() method of VirtualEnv to allow passing a new
    parameter pointing to the root of the pyenv installation. If present,
    a virtualenv will be created using the Python version specified by the
    "python" parameter. This string should match a valid Python version as
    it would be passed to "pyenv install".
    
    Co-authored-by: nre <nre@ableton.com>

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -9,3 +9,15 @@ import com.ableton.VirtualEnv
 VirtualEnv create(String python) {
   return VirtualEnv.create(this, python)
 }
+
+
+/**
+ * Create a new {@link com.ableton.VirtualEnv} instance using pyenv.
+ * @param python Python version, as given by pyenv versions --list.
+ * @param pyenvRoot Path of the pyenv installation. If {@code null}, then the pyenv root
+ *        will be detected from the environment in {@code PYENV_ROOT}.
+ * @return New {@link com.ableton.VirtualEnv} instance.
+ */
+VirtualEnv createWithPyenv(String python, String pyenvRoot = null) {
+  return VirtualEnv.create(this, python, pyenvRoot ?: env.PYENV_ROOT)
+}
