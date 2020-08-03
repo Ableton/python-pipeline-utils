38d9d9b88e8255c69a396c574746868c6c402a7a
nre <nre@ableton.com>
2020-08-03 13:24:40 +0200

    Cleanup: sort test methods

--- test/com/ableton/VirtualEnvTest.groovy
+++ test/com/ableton/VirtualEnvTest.groovy
@@ -31,85 +31,12 @@ class VirtualEnvTest extends BasePipelineTest {
   }
 
   @Test
-  void newObjectUnix() throws Exception {
-    if (JenkinsMocks.isUnix()) {
-      helper.registerAllowedMethod('isUnix', []) {
-        return true
-      }
+  void cleanup() throws Exception {
+    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
 
-      VirtualEnv venv = new VirtualEnv(script, 'python2.7')
+    VirtualEnv venv = new VirtualEnv(script, 'python3.6')
 
-      assertNotNull(venv)
-      assertNotNull(venv.script)
-      assertNotNull(venv.destDir)
-    }
-  }
-
-  @Test
-  void newObjectWindows() throws Exception {
-    if (!JenkinsMocks.isUnix()) {
-      script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
-      helper.registerAllowedMethod('isUnix', []) {
-        return false
-      }
-
-      VirtualEnv venv = new VirtualEnv(script, 'python2.7')
-
-      assertNotNull(venv)
-      assertNotNull(venv.script)
-      assertNotNull(venv.destDir)
-    }
-  }
-
-  @Test
-  void newObjectWithNullScript() throws Exception {
-    boolean exceptionThrown = false
-    try {
-      new VirtualEnv(null, 'python2.7')
-    } catch (AssertionError error) {
-      exceptionThrown = true
-      assertNotNull(error)
-    }
-    assertTrue(exceptionThrown)
-  }
-
-  @Test
-  void newObjectWithNullPython() throws Exception {
-    boolean exceptionThrown = false
-    try {
-      new VirtualEnv(script, null)
-    } catch (AssertionError error) {
-      exceptionThrown = true
-      assertNotNull(error)
-    }
-    assertTrue(exceptionThrown)
-  }
-
-  @Test
-  void newObjectWithAbsolutePath() throws Exception {
-    if (JenkinsMocks.isUnix()) {
-      String python = '/usr/bin/python3.5'
-
-      VirtualEnv venv = new VirtualEnv(script, python)
-
-      // Expect that the dirname of the python installation is stripped from the
-      // virtualenv directory, but that it still retains the correct python version.
-      assertFalse(venv.destDir.contains('usr/bin'))
-      assertTrue(venv.destDir.endsWith('python3.5'))
-    }
-  }
-
-  @Test
-  void newObjectWithAbsolutePathWindows() throws Exception {
-    if (!JenkinsMocks.isUnix()) {
-      script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
-      String python = '/c/Python27/python'
-
-      VirtualEnv venv = new VirtualEnv(script, python)
-
-      assertFalse(venv.destDir.startsWith('/c'))
-      assertTrue(venv.destDir.endsWith('python'))
-    }
+    venv.cleanup()
   }
 
   @Test
@@ -159,13 +86,6 @@ class VirtualEnvTest extends BasePipelineTest {
     VirtualEnv.create(script, '1.2.3', pyenvRoot)
   }
 
-  @Test(expected = Exception)
-  void createPyenvWindows() throws Exception {
-    helper.registerAllowedMethod('isUnix', []) { return false }
-
-    VirtualEnv.create(script, '1.2.3', 'C:\\pyenv')
-  }
-
   @Test(expected = AssertionError)
   void createPyenvNoRoot() throws Exception {
     helper.registerAllowedMethod('isUnix', []) { return true }
@@ -173,12 +93,92 @@ class VirtualEnvTest extends BasePipelineTest {
     VirtualEnv.create(script, '1.2.3', null)
   }
 
+  @Test(expected = Exception)
+  void createPyenvWindows() throws Exception {
+    helper.registerAllowedMethod('isUnix', []) { return false }
+
+    VirtualEnv.create(script, '1.2.3', 'C:\\pyenv')
+  }
+
   @Test
-  void cleanup() throws Exception {
-    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
+  void newObjectUnix() throws Exception {
+    if (JenkinsMocks.isUnix()) {
+      helper.registerAllowedMethod('isUnix', []) {
+        return true
+      }
 
-    VirtualEnv venv = new VirtualEnv(script, 'python3.6')
+      VirtualEnv venv = new VirtualEnv(script, 'python2.7')
 
-    venv.cleanup()
+      assertNotNull(venv)
+      assertNotNull(venv.script)
+      assertNotNull(venv.destDir)
+    }
+  }
+
+  @Test
+  void newObjectWindows() throws Exception {
+    if (!JenkinsMocks.isUnix()) {
+      script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
+      helper.registerAllowedMethod('isUnix', []) {
+        return false
+      }
+
+      VirtualEnv venv = new VirtualEnv(script, 'python2.7')
+
+      assertNotNull(venv)
+      assertNotNull(venv.script)
+      assertNotNull(venv.destDir)
+    }
+  }
+
+  @Test
+  void newObjectWithAbsolutePath() throws Exception {
+    if (JenkinsMocks.isUnix()) {
+      String python = '/usr/bin/python3.5'
+
+      VirtualEnv venv = new VirtualEnv(script, python)
+
+      // Expect that the dirname of the python installation is stripped from the
+      // virtualenv directory, but that it still retains the correct python version.
+      assertFalse(venv.destDir.contains('usr/bin'))
+      assertTrue(venv.destDir.endsWith('python3.5'))
+    }
+  }
+
+  @Test
+  void newObjectWithAbsolutePathWindows() throws Exception {
+    if (!JenkinsMocks.isUnix()) {
+      script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
+      String python = '/c/Python27/python'
+
+      VirtualEnv venv = new VirtualEnv(script, python)
+
+      assertFalse(venv.destDir.startsWith('/c'))
+      assertTrue(venv.destDir.endsWith('python'))
+    }
+  }
+
+  @Test
+  void newObjectWithNullPython() throws Exception {
+    boolean exceptionThrown = false
+    try {
+      new VirtualEnv(script, null)
+    } catch (AssertionError error) {
+      exceptionThrown = true
+      assertNotNull(error)
+    }
+    assertTrue(exceptionThrown)
+  }
+
+  @Test
+  void newObjectWithNullScript() throws Exception {
+    boolean exceptionThrown = false
+    try {
+      new VirtualEnv(null, 'python2.7')
+    } catch (AssertionError error) {
+      exceptionThrown = true
+      assertNotNull(error)
+    }
+    assertTrue(exceptionThrown)
   }
 }
