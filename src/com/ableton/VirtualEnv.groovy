Ableton/python-pipeline-utils

commit a89b2cf81ec2b7c65f5bba6df1fb65a7488fc4f5

Store virtualenvs in workspace, not temp folder
This feature was added in 5eaa6088 because the workspace directories
created by the GitHub Organization plugin could be very long, and
would cause problems since the length of the script shebang is limited
to 128 characters.

This change may cause problems for these users; hence, it is an API
breaking change and a version bump will be introduced in a subsequent
commit.

src/com/ableton/VirtualEnv.groovy
@@ -103,24 +103,21 @@ class VirtualEnv implements Serializable {
 
     this.script = script
 
-    String tempDir = '/tmp'
     if (script.isUnix()) {
       activateSubDir = 'bin'
     } else {
       activateSubDir = 'Scripts'
-      List tempDirParts = script.env.TEMP.split(':')
-      tempDir = "/${tempDirParts[0]}${tempDirParts[1].replace('\\', '/')}"
     }
 
-    this.destDir = "${tempDir}/${script.env.JOB_BASE_NAME}/${script.env.BUILD_NUMBER}/" +
-      python.split('/').last()
-
+    this.destDir = "${script.env.WORKSPACE}/.venv/${python.split('/').last()}"
     this.activateCommands = ". ${destDir}/${activateSubDir}/activate"
   }
 
   /**
-   * Removes the Virtualenv from disk. You should call this method in the cleanup stage
-   * of the pipeline to avoid cluttering the build node with temporary files.
+   * Removes the Virtualenv from disk. You can call this method in the cleanup stage of
+   * the pipeline to avoid cluttering the build node with temporary files. Note that the
+   * virtualenv is stored underneath the workspace, so removing the workspace will have
+   * the same effect.
    */
   void cleanup() {
     script.dir(destDir) {

test/com/ableton/VirtualEnvTest.groovy
@@ -24,7 +24,7 @@ class VirtualEnvTest extends BasePipelineTest {
 
     this.script = loadScript('test/resources/EmptyPipeline.groovy')
     assertNotNull(script)
-    script.env = ['BUILD_NUMBER': 1, 'JOB_BASE_NAME': 'mock']
+    script.env = ['WORKSPACE': '/workspace']
 
     helper.registerAllowedMethod('error', [String]) { message ->
       throw new Exception(message)
@@ -33,8 +33,6 @@ class VirtualEnvTest extends BasePipelineTest {
 
   @Test
   void cleanup() {
-    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
-
     VirtualEnv venv = new VirtualEnv(script, 'python3.6')
 
     venv.cleanup()
@@ -42,7 +40,6 @@ class VirtualEnvTest extends BasePipelineTest {
 
   @Test
   void create() {
-    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
     String python = 'python2.7'
 
     VirtualEnv venv = new VirtualEnv(script, python)
@@ -70,7 +67,7 @@ class VirtualEnvTest extends BasePipelineTest {
       pyenv install --skip-existing ${pythonVersion}
       pyenv shell ${pythonVersion}
       pip install virtualenv
-      virtualenv /tmp/mock/1/${pythonVersion}
+      virtualenv /workspace/${pythonVersion}
     """, '', 0)
 
     VirtualEnv venv = VirtualEnv.create(script, pythonVersion, pyenvRoot)
@@ -114,7 +111,6 @@ class VirtualEnvTest extends BasePipelineTest {
 
   @Test
   void newObjectWindows() {
-    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
     helper.registerAllowedMethod('isUnix', []) { return false }
 
     VirtualEnv venv = new VirtualEnv(script, 'python2.7')
@@ -140,7 +136,6 @@ class VirtualEnvTest extends BasePipelineTest {
   @Test
   void newObjectWithAbsolutePathWindows() {
     helper.registerAllowedMethod('isUnix', []) { return false }
-    script.env['TEMP'] = 'C:\\Users\\whatever\\AppData\\Temp'
     String python = '/c/Python27/python'
 
     VirtualEnv venv = new VirtualEnv(script, python)
@@ -176,7 +171,7 @@ class VirtualEnvTest extends BasePipelineTest {
   @Test
   void run() {
     String mockScriptCall = '''
-      . /tmp/mock/1/python/bin/activate
+      . /workspace/.venv/python/bin/activate
       mock-script
     '''
     helper.addShMock(mockScriptCall, 'mock output', 0)
@@ -192,7 +187,7 @@ class VirtualEnvTest extends BasePipelineTest {
   @Test
   void runWithMap() {
     String mockScriptCall = '''
-      . /tmp/mock/1/python/bin/activate
+      . /workspace/.venv/python/bin/activate
       mock-script
     '''
     helper.addShMock(mockScriptCall, 'mock output', 0)
@@ -208,7 +203,7 @@ class VirtualEnvTest extends BasePipelineTest {
   @Test
   void runWithMapReturnStatus() {
     String mockScriptCall = '''
-      . /tmp/mock/1/python/bin/activate
+      . /workspace/.venv/python/bin/activate
       mock-script
     '''
     helper.addShMock(mockScriptCall, 'mock output', 1234)
@@ -226,7 +221,7 @@ class VirtualEnvTest extends BasePipelineTest {
   @Test
   void runWithMapReturnStdout() {
     String mockScriptCall = '''
-      . /tmp/mock/1/python/bin/activate
+      . /workspace/.venv/python/bin/activate
       mock-script
     '''
     helper.addShMock(mockScriptCall, 'mock output', 0)

src/com/ableton/VirtualEnvList.groovy
@@ -37,8 +37,10 @@ class VirtualEnvList implements Serializable {
   }
 
   /**
-   * Cleanup all Virtualenv files on disk. You should call this method in the cleanup
-   * stage of the pipeline to avoid cluttering the build node with temporary files.
+   * Cleanup all Virtualenv files on disk. You can call this method in the cleanup stage
+   * of the pipeline to avoid cluttering the build node with temporary files. Note that
+   * the virtualenv is stored underneath the workspace, so removing the workspace will
+   * have the same effect.
    */
   void cleanup() {
     venvs.each { venv ->

commit c4753acc56ad6ecc718c5733840bf57a35d702a2

Fix virtualenv path delimiters on Windows
This was broken in a89b2cf, but as our unit tests only checked to see
if the destDir was non-null, this bug slipped into master. Thus, this
test also improves the tests for creating new VirtualEnv objects to
check the actual destDir contents.

src/com/ableton/VirtualEnv.groovy
@@ -102,15 +102,17 @@ class VirtualEnv implements Serializable {
     assert script
 
     this.script = script
+    String workspace = script.env.WORKSPACE
 
     if (script.isUnix()) {
       activateSubDir = 'bin'
     } else {
       activateSubDir = 'Scripts'
+      workspace = workspace.replace('\\', '/')
     }
 
     long seed = randomSeed ?: System.currentTimeMillis() * this.hashCode()
-    this.destDir = "${script.env.WORKSPACE}/.venv/venv-${randomName(seed)}"
+    this.destDir = "${workspace}/.venv/venv-${randomName(seed)}"
     this.activateCommands = ". ${destDir}/${activateSubDir}/activate"
   }
 

test/com/ableton/VirtualEnvTest.groovy
@@ -102,22 +102,25 @@ class VirtualEnvTest extends BasePipelineTest {
   void newObjectUnix() {
     helper.registerAllowedMethod('isUnix', []) { return true }
 
-    VirtualEnv venv = new VirtualEnv(script)
+    VirtualEnv venv = new VirtualEnv(script, 1)
 
     assertNotNull(venv)
     assertNotNull(venv.script)
     assertNotNull(venv.destDir)
+    assertEquals('/workspace/.venv/venv-58734446', venv.destDir)
   }
 
   @Test
   void newObjectWindows() {
     helper.registerAllowedMethod('isUnix', []) { return false }
+    script.env.WORKSPACE = 'C:\\workspace'
 
-    VirtualEnv venv = new VirtualEnv(script)
+    VirtualEnv venv = new VirtualEnv(script, 1)
 
     assertNotNull(venv)
     assertNotNull(venv.script)
     assertNotNull(venv.destDir)
+    assertEquals('C:/workspace/.venv/venv-58734446', venv.destDir)
   }
 
   @Test