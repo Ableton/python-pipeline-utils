cffc443d6af79325b4da21a2d6473d7ed0ee12e3
nre <nre@ableton.com>
2022-12-20 16:01:35 +0100

    Fix test for Pyenv.createVirtualEnv
    Previously, we only cared that this method didn't throw, but that led to
    the situation where a misconfigured mock meant that this test silently
    didn't do anything. This commit fixes that mock and makes the test
    actually ensure that the VirtualEnv was created correctly.
    
    Should have been done in d4fe862.
    
    Make test for createVirtualEnv assert something

--- test/com/ableton/PyenvTest.groovy
+++ test/com/ableton/PyenvTest.groovy
@@ -1,5 +1,6 @@
 package com.ableton
 
+import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.junit.jupiter.api.Assertions.assertFalse
 import static org.junit.jupiter.api.Assertions.assertNotNull
 import static org.junit.jupiter.api.Assertions.assertThrows
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.Test
 
 
 class PyenvTest extends BasePipelineTest {
+  // Expected random virtualenv directory name for the seed value of 1
+  final static String TEST_RANDOM_NAME = 'venv-58734446'
+
   Object script
 
   @Override
@@ -50,17 +54,21 @@ class PyenvTest extends BasePipelineTest {
     String pyenvRoot = '/mock/pyenv/root'
     helper.registerAllowedMethod('fileExists', [String]) { return true }
     helper.registerAllowedMethod('isUnix', []) { return true }
+    // Indentation must match the actual command
     helper.addShMock("""
-      export PYENV_ROOT=${pyenvRoot}
-      export PATH=\$PYENV_ROOT/bin:\$PATH
-      eval "\$(pyenv init -)"
-      pyenv install --skip-existing ${pythonVersion}
-      pyenv shell ${pythonVersion}
-      pip install virtualenv
-      virtualenv /workspace/${pythonVersion}
-    """, '', 0)
+        export PYENV_ROOT=${pyenvRoot}
+        export PATH=\$PYENV_ROOT/bin:\$PATH
+        eval "\$(pyenv init --path)"
+        eval "\$(pyenv init -)"
+        pyenv install --skip-existing ${pythonVersion}
+        pyenv shell ${pythonVersion}
+        pip install virtualenv
+        virtualenv /workspace/.venv/${TEST_RANDOM_NAME}
+      """, '', 0)
 
-    new Pyenv(script, pyenvRoot).createVirtualEnv(pythonVersion)
+    Object venv = new Pyenv(script, pyenvRoot).createVirtualEnv(pythonVersion, 1)
+
+    assertEquals("/workspace/.venv/${TEST_RANDOM_NAME}" as String, venv.venvRootDir)
   }
 
   @Test
